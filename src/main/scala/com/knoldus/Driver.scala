21416850a568a50ac7339dfe29a22e9ad755c57c
Manish Mishra <manish.mishra@knoldus.com>
2023-04-12 13:41:27 +0530

    Added Driver.scala file

--- src/main/scala/com/knoldus/Driver.scala
+++ src/main/scala/com/knoldus/Driver.scala
@@ -6,7 +6,7 @@ import scala.util.{Failure, Success, Try}
 /* This is Driver class to check the program is running or not */
 object Driver extends App {
   private val averageCalculator = new AverageCalculator
-  private val filePath = "/home/knoldus/Desktop/KUP/Asignment/SCALA SESSION/session-5/assignment-1/src/main/scala/com/knoldus/StudentsDetails.csv"
+  private val filePath = "/home/knoldus/Desktop/KUP/Assignment/SCALA SESSION/session-5/assignment-1/src/main/scala/com/knoldus/StudentsDetails.csv"
   val result = Try(averageCalculator.calculateGrades(filePath))
   result match {
     case Success(average) => average.onComplete {
